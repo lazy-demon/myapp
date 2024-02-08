bee0d813fd18297bda38b4705bfef9167f543cb4
jefdayt <jefdayt@gmail.com>
2024-02-08 08:38:28 +0000

    cleanup

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,25 +3,37 @@
   packages = [
     pkgs.flutter
     pkgs.jdk17
+    pkgs.docker
   ];
-  idx.extensions = [];
+  services.docker.enable = true;
+  idx.extensions = [ ];
+  idx.workspace.onStart = {
+    compose = "cd slash_server && docker compose up --build --detach";
+  };
+
   idx.previews = {
     enable = true;
     previews = [
-        {
-            command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
-            id = "web";
-            manager = "flutter";
-        }
-        {
-            command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
-            id = "android";
-            manager = "flutter";
-        }
-        {
-            id = "ios";
-            manager = "ios";
-        }
+      # {
+      #   cwd = "slash_server";
+      #   command = [ "dart" "bin/main.dart" "--apply-migrations" ];
+      #   id = "web";
+      #   manager = "web";
+      # }
+      # {
+      #     command = ["cd" "slash_flutter" "&&" "flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+      #     id = "web";
+      #     manager = "flutter";
+      # }
+      # {
+      #     command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+      #     id = "android";
+      #     manager = "flutter";
+      # }
+      # {
+      #     id = "ios";
+      #     manager = "ios";
+      # }
     ];
   };
 }
